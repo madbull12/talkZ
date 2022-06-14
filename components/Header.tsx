41b64d08993c627e12f86962df51d75d16542181
madbull12 <huangandrian02@gmail.com>
2022-06-14 10:15:38 +0700

    another changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,18 @@
 import { IoIosNotificationsOutline } from "react-icons/io"
 import Search from "./Search"
+import { signIn, signOut, useSession } from "next-auth/react"
+import Image from "next/image"
+
+interface ISession {
+    name:string;
+    image:string;
+    email:string;
+}
+
 
 const Header = () => {
+    const { data: session } = useSession()
+    console.log(session)
   return (
     //header component
     <nav className="sm:p-[12px] p-2 md:p-4  border-b border-gray-800">
@@ -14,9 +25,21 @@ const Header = () => {
             <Search />
             <div className="flex items-center md:gap-x-2">
                 <IoIosNotificationsOutline className="text-gray-400 lg md:text-2xl" />
-                <button className="text-gray-400 hidden md:flex font-semibold text-xs sm:text-sm bg-[#1E293B] border-t rounded-xl whitespace-nowrap border-gray-700 md:text-base px-2 md:px-4 p-[4px] sm:py-2 shadow-md">
-                Sign in 
-            </button>
+                {/* <p>{session?.user?.name}</p> */}
+                {session ? (
+                    <>
+                        <Image  src={session?.user?.image} alt={session?.user?.name} width={35} height={35} className="w-10 h-10 rounded-full" objectFit="cover" />
+                        <button onClick={() => signOut()} className="text-gray-400 hidden md:flex font-semibold text-xs sm:text-sm bg-[#1E293B] border-t rounded-xl whitespace-nowrap border-gray-700 md:text-base px-2 md:px-4 p-[4px] sm:py-2 shadow-md">
+                            Sign Out
+                        </button>
+                    </>
+                 
+                ):(
+                    <button onClick={() => signIn()} className="text-gray-400 hidden md:flex font-semibold text-xs sm:text-sm bg-[#1E293B] border-t rounded-xl whitespace-nowrap border-gray-700 md:text-base px-2 md:px-4 p-[4px] sm:py-2 shadow-md">
+                     Sign in 
+                    </button>
+                )}
+               
             </div>
         </div>
         
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
+import Header from '../components/Header'
 
 function MyApp({ Component, pageProps:{ session,...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <main className='bg-[#0F172A] min-h-screen'>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+    
 
     </SessionProvider>
   )
