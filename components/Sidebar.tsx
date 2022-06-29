ed17528b6b51b7f78613da9acbcb4b8495579750
madbull12 <huangandrian02@gmail.com>
2022-06-29 19:54:09 +0700

    working on responsive design and fixing some b ugs

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,30 +6,40 @@ import { MdOutlineExplore,MdOutlineQuestionAnswer } from 'react-icons/md'
 const Sidebar = () => {
   const router = useRouter()
   return (
-    <div className="fixed w-[220px] space-y-3  lg:block  top-16 left-0  h-screen bg-transparent  px-4 pt-8 z-50 text-gray-400">
-      <p className="text-sm font-bold">MENU</p>
-      <ul className="lg:space-y-3 flex lg:flex-col">
-        <li className={` flex items-center gap-x-2 ${router.pathname === "/" ? "text-[#0EA5E9]  font-bold" : ""}`}>
-          <AiOutlineHome className="text-3xl lg:text-xl " />
-          <Link href="/"  >
-            <span className="hidden lg:block">Home</span>
-          </Link>
-        </li>
-        <li className={`flex items-center gap-x-2 ${router.pathname === "/explore" ? "text-[#0EA5E9] font-bold" : ""}`}>
-          <MdOutlineExplore  className="text-2xl lg:text-xl " />
-          <Link href="/explore">
+    <div className="fixed w-full lg:w-[220px] space-y-2 lg:block top-4 md:top-10 lg:top-16 lg:h-screen  px-4 py-8 z-[100] text-gray-400 bg-transparent">
+      <p className="text-sm font-bold hidden lg:block">MENU</p>
+      <ul className="lg:space-y-3 flex lg:flex-col w-full justify-center space-x-8 lg:space-x-0">
+         
+        <Link href="/">
+          <span className={` flex items-center cursor-pointer gap-x-2 ${router.pathname === "/" ? "text-[#0EA5E9]  font-bold" : ""}`}>
+            <AiOutlineHome className="text-3xl lg:text-xl " />
+            <span className="hidden lg:block">Home</span>           
+          </span>
+
+        </Link>
+    
+    
+        <Link href="/explore">
+          <span className={`flex cursor-pointer items-center gap-x-2 ${router.pathname === "/explore" ? "text-[#0EA5E9] font-bold" : ""}`}>
+            <MdOutlineExplore  className="text-2xl lg:text-xl " />
             <span  className=" hidden lg:block">Explore topics</span>
-          </Link>
-        </li >
-        <li className={`flex items-center gap-x-2 `}>
-          <MdOutlineQuestionAnswer className="text-3xl lg:text-xl" />
-          <Link href="/" >
-            
-            <span  className=" hidden lg:block">
-              My answers
-            </span>
-          </Link>
-        </li >
+
+          </span>
+
+        </Link>
+  
+
+        <Link href="/">
+          <span className={`flex items-center gap-x-2 cursor-pointer`}>
+              <MdOutlineQuestionAnswer className="text-3xl lg:text-xl" />
+              
+              <span  className=" hidden lg:block">
+                My answers
+              </span>
+          </span>
+        </Link>
+      
+  
         
       </ul>
       
